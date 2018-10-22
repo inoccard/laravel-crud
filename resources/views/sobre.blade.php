90de74f3f2f3aa0301390ad52903c6ef33aca843
Inoccard <inocenciocardoso19@gmail.com>
2018-10-21 21:50:06 -0300

    laravel crud

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -13,8 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    Tec Genius
                 </div>
             </div>
         </div>
