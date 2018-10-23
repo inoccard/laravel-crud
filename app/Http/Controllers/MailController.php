bd67b6cc323fa5e0aaadc42ada3cc72b08c30c7c
Inoccard <inocenciocardoso19@gmail.com>
2018-10-23 19:37:25 -0300

    Controlador de E-mail

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,125 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use DummyFullModelClass;
+use App\lain;
+use Illuminate\Http\Request;
+
+class MailController extends Controller
+{
+
+    public function basic_email()
+    {
+        $data = array('name' => "Inocêncio Cardoso");
+
+        \Mail::send(['text' => 'mail'], $data, function ($message) {
+            $message->to('10enraska@gmail.com', 'Laravel Email')->subject('Laravel Basic Testing Mail');
+            $message->from('inocenciocardoso19@gmail.com', 'Inocêncio Cardoso');
+        });
+        echo "Basic Email Sent. Check your inbox.";
+    }
+    public function html_email()
+    {
+        $data = array('name' => "Inocêncio Cardoso");
+        \Mail::send('mail', $data, function ($message) {
+            $message->to('10enraska@gmail.com', 'Laravel Email')->subject('Laravel HTML Testing Mail');
+            $message->from('inocenciocardoso19@gmail.com', 'Inocêncio Cardoso');
+        });
+        echo "HTML Email Sent. Check your inbox.";
+    }
+    public function attachment_email()
+    {
+        $data = array('name' => "Inocêncio Cardoso");
+        \Mail::send('mail', $data, function ($message) {
+            $message->to('10enraska@gmail.com', 'Laravel Email')->subject('Laravel Testing Mail with Attachment');
+            $message->attach('C:\laravel-master\laravel\public\uploads\image.png');
+            $message->attach('C:\laravel-master\laravel\public\uploads\test.txt');
+            $message->from('inocenciocardoso19@gmail.com', 'Inocêncio Cardoso');
+        });
+        echo "Email Sent with attachment. Check your inbox.";
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @param  \App\lain  $lain
+     * @return \Illuminate\Http\Response
+     */
+    public function index(lain $lain)
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param  \App\lain  $lain
+     * @return \Illuminate\Http\Response
+     */
+    public function create(lain $lain)
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\lain  $lain
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, lain $lain)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\lain  $lain
+     * @param  \DummyFullModelClass  $DummyModelVariable
+     * @return \Illuminate\Http\Response
+     */
+    public function show(lain $lain, DummyModelClass $DummyModelVariable)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\lain  $lain
+     * @param  \DummyFullModelClass  $DummyModelVariable
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(lain $lain, DummyModelClass $DummyModelVariable)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\lain  $lain
+     * @param  \DummyFullModelClass  $DummyModelVariable
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, lain $lain, DummyModelClass $DummyModelVariable)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\lain  $lain
+     * @param  \DummyFullModelClass  $DummyModelVariable
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(lain $lain, DummyModelClass $DummyModelVariable)
+    {
+        //
+    }
+}
