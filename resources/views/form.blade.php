88b69d407a2f975f93cec62f31f844bf4e6cacf4
Inoccard <inocenciocardoso19@gmail.com>
2018-10-24 00:06:34 -0300

    formulário básico

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,39 +1,42 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Formulário</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-      <?php
-        echo Form::open(array('url' => 'foo/bar'));
-        echo 'Nome: <br>' . Form::text('username');
-        echo '<br/>';
+@extends('layouts.lform')
 
-        echo 'E-mail:<br> ' . Form::text('email');
-        echo '<br/>';
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Envie seu formulário</div>
 
-        echo 'Senha:<br> ' . Form::password('password');
-        echo '<br/>';
+                <div class="card-body">
+                <?php
+                echo Form::open(array('url' => 'foo/bar'));
+                echo 'Nome: <br>' . Form::text('username');
+                echo '<br/>';
 
-        echo Form::checkbox('name', 'value');
-        echo '<br/>';
+                echo 'E-mail:<br> ' . Form::text('email');
+                echo '<br/>';
 
-        echo Form::radio('name', 'value');
-        echo '<br/>';
+                echo 'Senha:<br> ' . Form::password('password');
+                echo '<br/>';
 
-        echo Form::file('image');
-        echo '<br/>';
+                echo Form::checkbox('name', 'value');
+                echo '<br/>';
 
-        echo Form::select('size', array('L' => 'Large', 'S' => 'Small'));
-        echo '<br/>';
+                echo Form::radio('name', 'value');
+                echo '<br/>';
 
-        echo Form::submit('Clique aqui!');
-        echo Form::close();
-        ?>
-</body>
-</html>
+                echo Form::file('image');
+                echo '<br/>';
+
+                echo Form::select('size', array('L' => 'Large', 'S' => 'Small'));
+                echo '<br/>';
+
+                echo Form::submit('Clique aqui!');
+                echo Form::close();
+                ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
