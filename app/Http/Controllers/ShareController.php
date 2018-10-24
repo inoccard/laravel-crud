7891d144d2c1248a919113332d45035a2653c12f
Inoccard <inocenciocardoso19@gmail.com>
2018-10-23 22:23:05 -0300

    Share Controller
    adição de autenticação na página share, só pode ver quem estiver autenticado

--- app/Http/Controllers/ShareController.php
+++ app/Http/Controllers/ShareController.php
@@ -7,6 +7,17 @@ use App\Share;
 
 class ShareController extends Controller
 {
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth', 'verified']);
+    }
+
     /**
      * Display a listing of the resource.
      *
