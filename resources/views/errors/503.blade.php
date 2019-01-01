d1fa02942c0d2b8d99f8ad6149285e5dfe3648dd
inoccard <inocenciocardoso19@gmail.com>
2019-01-01 14:25:13 -0200

    página de erro para manutenção

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+ 
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h1>Desculpe, estamos fazendo alguma manutenção. Por favor, volte em breve.</h1>
+            </div>
+        </div>
+    </div>
+@endsection
