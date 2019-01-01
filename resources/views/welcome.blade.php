c1bd8cce12657bf6559a174f277391e685e431d4
inoccard <inocenciocardoso19@gmail.com>
2019-01-01 11:42:52 -0200

    welcome site

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,10 +69,11 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{url('/shares')}}">Shares</a>
+                        <a href="{{ url('/home') }}">Sistema</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
+                        <a href="{{ route('register') }}">Cadastro</a>
                     @endauth
                 </div>
             @endif
