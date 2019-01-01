96e3087124694fbcb83e2818d7068c4dd2c17c48
inoccard <inocenciocardoso19@gmail.com>
2019-01-01 14:27:24 -0200

    rotas removidas

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,6 @@ Route::get('/', function () {
 
 Route::resource('shares', "ShareController");
 
-// Second Route method – Root URL with ID will match this method
-Route::get('ID/{id}', function ($id) {
-    echo 'ID: ' . $id;
-});
-// Third Route method – Root URL with or without name will match this method
-Route::get('/user/{name?}', function ($name = 'Inocêncio Cardoso') {
-    echo "Name: " . $name;
-});
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
@@ -34,5 +26,3 @@ Route::get('/sobre', 'SobreController@sobre')->name('sobre');
 Route::get('sendbasicemail', 'MailController@basic_email');
 Route::get('sendhtmlemail', 'MailController@html_email');
 Route::get('sendattachmentemail', 'MailController@attachment_email');
-
-Route::get('/form', 'FormController@index')->name('form');
